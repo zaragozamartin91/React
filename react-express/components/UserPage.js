800d1644b248d6e5dc442875c769c4be05cf9b3f
Martin <zaragozamartin91@gmail.com>
2017-06-28 15:56:24 -0300

    Se configuro el proyecto para compilar un bundle por cada vista de react

--- react-express/components/UserPage.js
+++ react-express/components/UserPage.js
@@ -1,5 +1,7 @@
 import users from '../data/users';
 import React from 'react';
+import ReactDom from 'react-dom';
+
 import User from './User';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyAwesomeReactComponent from './MyAwesomeReactComponent';
@@ -17,4 +19,7 @@ var UserPage = React.createClass({
     }
 });
 
-export default UserPage;
+ReactDom.render(
+    <UserPage />,
+    document.getElementById('root')
+);
