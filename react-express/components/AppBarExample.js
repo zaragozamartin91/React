677054c3c0a6e81c3c697ec210bf4a20821b9065
Martin <zaragozamartin91@gmail.com>
2017-06-28 16:35:34 -0300

    Se mejoro el ejemplo de AppBar

--- react-express/components/AppBarExample.js
+++ react-express/components/AppBarExample.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
 
 /* ESTE FRAGMENTO DE CODIGO ES REQUERIDO PARA LOS EVENTOS DE TIPO TOUCH O CLICK EN COMPONENTES MATERIAL-UI */
 var injectTapEventPlugin = require("react-tap-event-plugin");
@@ -10,20 +11,25 @@ injectTapEventPlugin();
 
 
 const MyAppBar = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {
-            
+            drawerOpen: false
         }
     },
 
-    appBarLeftTap: function() {
-        
+    appBarLeftTap: function () {
+        console.log('AppBar tap!');
+        const drawerOpen = this.state.drawerOpen;
+        this.setState({ drawerOpen: !drawerOpen });
     },
 
     render: function () {
         return (
             <MuiThemeProvider>
-                <AppBar onLeftIconButtonTouchTap={this.appBarLeftTap} />
+                <div>
+                    <AppBar onLeftIconButtonTouchTap={this.appBarLeftTap} />
+                    <Drawer open={this.state.drawerOpen} docked={false} />
+                </div>
             </MuiThemeProvider>
         );
     }
